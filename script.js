298ad9c5e217335c4010dd512e2e2c8d36514b6f
Mateus Alves <matheus.spg01@hotmail.com>
2026-05-30 19:56:20 -0300

    Adicionado localStorage e status coloridos

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let candidaturas = [];
+let candidaturas =
+JSON.parse(localStorage.getItem("candidaturas")) || [];
 
-function adicionarCandidatura(){
+atualizarLista();
+
+function adicionarCandidatura() {
 
     let vaga =
     document.getElementById("vaga").value;
@@ -17,12 +20,12 @@ function adicionarCandidatura(){
     let status =
     document.getElementById("status").value;
 
-    if(
+    if (
         vaga === "" ||
         empresa === "" ||
         data === "" ||
         hora === ""
-    ){
+    ) {
         alert("Preencha todos os campos");
         return;
     }
@@ -35,32 +38,85 @@ function adicionarCandidatura(){
         status
     });
 
+    salvarDados();
     atualizarLista();
+
+    document.getElementById("vaga").value = "";
+    document.getElementById("empresa").value = "";
+    document.getElementById("data").value = "";
+    document.getElementById("hora").value = "";
 }
 
-function atualizarLista(){
+function atualizarLista() {
 
     let lista =
     document.getElementById("lista");
 
     lista.innerHTML = "";
 
-    candidaturas.forEach(item => {
+    candidaturas.forEach((item, index) => {
 
         let li =
         document.createElement("li");
+
+        let corStatus = "";
+
+switch(item.status){
+    case "Enviado":
+        corStatus = "gold";
+        break;
+
+    case "Em análise":
+        corStatus = "dodgerblue";
+        break;
+
+    case "Entrevista":
+        corStatus = "orange";
+        break;
+
+    case "Aprovado":
+        corStatus = "green";
+        break;
+
+    case "Reprovado":
+        corStatus = "red";
+        break;
+}
 
         li.innerHTML = `
             <strong>${item.vaga}</strong><br>
             Empresa: ${item.empresa}<br>
             Data: ${item.data}<br>
             Horário: ${item.hora}<br>
-            Status: ${item.status}
+            Status:
+<span style="color:${corStatus}; font-weight:bold;">
+    ${item.status}
+</span>
+
+            <button onclick="removerCandidatura(${index})">
+                Excluir
+            </button>
         `;
 
         lista.appendChild(li);
     });
 
     document.getElementById("contador")
-    .textContent = candidaturas.length;
+        .textContent = candidaturas.length;
+}
+
+function removerCandidatura(index) {
+
+    candidaturas.splice(index, 1);
+
+    salvarDados();
+    atualizarLista();
+}
+
+function salvarDados() {
+
+    localStorage.setItem(
+        "candidaturas",
+        JSON.stringify(candidaturas)
+    );
 }
